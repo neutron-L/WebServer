b50705aa2f752c60ea2099820c10649401a1bf45
rda <2055017970@qq.com>
2023-12-14 19:34:09 +0800

    locker: init and implement Locker

--- locker/locker.hh
+++ locker/locker.hh
@@ -4,24 +4,29 @@
 #include <iostream>
 #include <exception>
 #include <semaphore.h>
+#include <pthread.h>
 #include <queue>
 
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
 class Sem
 {
 private:
     sem_t sem;
+
 public:
     Sem() : Sem(0)
-    {   
+    {
     }
     Sem(unsigned int val)
     {
-if (sem_init(&sem, 0, val) != 0)
+        if (sem_init(&sem, 0, val) != 0)
+        {
+            perror("sem_init");
             throw std::exception();
+        }
     }
 
     ~Sem()
@@ -40,5 +45,35 @@ if (sem_init(&sem, 0, val) != 0)
     }
 };
 
+class Locker
+{
+private:
+    pthread_mutex_t lock;
+
+public:
+    Locker()
+    {
+        if (pthread_mutex_init(&lock, NULL) != 0)
+        {
+            perror("pthread_mutex_init");
+            throw std::exception();
+        }
+    }
+
+    ~Locker()
+    {
+        pthread_mutex_destroy(&lock);
+    }
+
+    bool lock()
+    {
+        return pthread_mutex_lock(&lock) == 0;
+    }
+
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&lock) == 0;
+    }
+};
 
 #endif
