3311a23f0ae1d2bce75bae0c8f57454052f71c51
rda <2055017970@qq.com>
2023-12-23 16:03:29 +0800

    thread pool: init

---
+++ threadpool/threadpool.hh
@@ -0,0 +1,21 @@
+#ifndef THREADPOOL_HH_
+#define THREADPOOL_HH_
+
+#include <iostream>
+#include <exception>
+#include <pthread.h>
+#include <queue>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+template <typename T>
+class ThreadPool
+{
+private:
+    
+};
+
+
+#endif
