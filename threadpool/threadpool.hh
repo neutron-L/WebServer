6066b1ff8a010e23b91807638f7e84735cb455e5
rda <2055017970@qq.com>
2023-12-25 20:25:27 +0800

    thread pool: add a simple thread pool, further optimization is needed

--- threadpool/threadpool.hh
+++ threadpool/threadpool.hh
@@ -9,8 +9,8 @@
 
 #include "locker.hh"
 
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
 /// @brief 线程池类
@@ -19,16 +19,16 @@ template <typename T>
 class ThreadPool
 {
 private:
-    uint32_t m_thread_number{};       // 线程数
-    uint32_t m_max_requests{};        // 待处理的最大任务数
-    std::vector<pthread_t> m_threads{};   // 线程数组
-    std::queue<T *> m_jobqueue{};     // 工作队列
+    uint32_t m_thread_number{};         // 线程数
+    uint32_t m_max_requests{};          // 待处理的最大任务数
+    std::vector<pthread_t> m_threads{}; // 线程数组
+    std::queue<T *> m_jobqueue{};       // 工作队列
     Locker m_queue_locker{};
-    Semaphore has_job{};           // 信号量——是否有任务加入工作队列
-    bool running{};   // 线程池是否正在运行
+    Semaphore has_job{}; // 信号量——是否有任务加入工作队列
+    bool running{};      // 线程池是否正在运行
 public:
-    ThreadPool(uint32_t nb_threads = 5, uint32_t max_nb_requests = 100) 
-    :m_thread_number(nb_threads), m_max_requests(max_nb_requests), m_threads(nb_threads)
+    ThreadPool(uint32_t nb_threads = 5, uint32_t max_nb_requests = 100)
+        : m_thread_number(nb_threads), m_max_requests(max_nb_requests), m_threads(nb_threads)
     {
         for (int i = 0; i < nb_threads; ++i)
         {
@@ -45,12 +45,56 @@ public:
         running = false;
     }
 
-    bool append(T * job)
+    bool append(T *job)
     {
-
+        m_queue_locker.acquire();
+        if (m_jobqueue.size() == m_max_requests)
+        {
+            m_queue_locker.release();
+            return false;
+        }
+        m_jobqueue.push(job);
+        m_queue_locker.release();
+        return true;
     }
 
-};
+    T *pull_job()
+    {
+        T *job{NULL};
 
+        m_queue_locker.acquire();
+        if (!m_jobqueue.empty() || !())
+        {
+            job = m_jobqueue.front()
+                      m_jobqueue.pop_front();
+        }
+        m_queue_locker.release();
+        return job;
+    }
+
+    void * worker(void *arg)
+    {
+        ThreadPool * pool = static_cast<ThreadPool *>(arg);
+        pool->run();
+
+        return NULL;
+    }
+
+    void stop()
+    {
+        running = false;
+    }
+
+    void run()
+    {
+        while (running)
+        {
+            has_job.wait();
+            T * job = pull_job();
+            if (job)
+                job->process();
+        }
+    }
+};
 
 #endif
