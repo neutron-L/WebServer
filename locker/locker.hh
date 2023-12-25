f27e080ea5c874c0afe431c0e9c5408e68c8a413
rda <2055017970@qq.com>
2023-12-25 20:06:52 +0800

    implement Semaphore Locker Condition wrapper class

--- locker/locker.hh
+++ locker/locker.hh
@@ -11,16 +11,16 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-class Sem
+class Semaphore
 {
 private:
     sem_t sem;
 
 public:
-    Sem() : Sem(0)
+    Semaphore() : Semaphore(0)
     {
     }
-    Sem(unsigned int val)
+    Semaphore(unsigned int val)
     {
         if (sem_init(&sem, 0, val) != 0)
         {
@@ -29,7 +29,7 @@ public:
         }
     }
 
-    ~Sem()
+    ~Semaphore()
     {
         sem_destroy(&sem);
     }
@@ -65,14 +65,60 @@ public:
         pthread_mutex_destroy(&lock);
     }
 
-    bool lock()
+    bool acquire()
     {
         return pthread_mutex_lock(&lock) == 0;
     }
 
-    bool unlock()
+    bool release()
     {
         return pthread_mutex_unlock(&lock) == 0;
+    }
+};
+
+class Condition
+{
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+
+public:
+    Condition()
+    {
+        if (pthread_mutex_init(&mutex, NULL) != 0)
+        {
+            perror("pthread_mutex_init");
+            throw std::exception();
+        }
+        if (pthread_cond_init(&cond, NULL) != 0)
+        {
+            perror("pthread_cond_init");
+            throw std::exception();
+        }
+    }
+    ~Condition()
+    {
+        pthread_mutex_destroy(&mutex);
+        pthread_cond_destroy(&cond);
+    }
+
+    bool wait()
+    {
+        int ret;
+        pthread_mutex_lock(&mutex);
+        ret = pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_unlock(&mutex);
+        return ret==0;
+    }
+
+    bool signal()
+    {
+        return pthread_cond_signal(&cond) == 0;
+    }
+
+    bool signalall()
+    {
+        return pthread_cond_broadcast(&cond) == 0;
     }
 };
 
--- threadpool/threadpool.hh
+++ threadpool/threadpool.hh
@@ -4,17 +4,52 @@
 #include <iostream>
 #include <exception>
 #include <pthread.h>
+#include <vector>
 #include <queue>
+
+#include "locker.hh"
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
+/// @brief 线程池类
+/// @tparam T 线程池中维护的工作队列中的待处理任务类型，需要提供某些接口供线程调用
 template <typename T>
 class ThreadPool
 {
 private:
-    
+    uint32_t m_thread_number{};       // 线程数
+    uint32_t m_max_requests{};        // 待处理的最大任务数
+    std::vector<pthread_t> m_threads{};   // 线程数组
+    std::queue<T *> m_jobqueue{};     // 工作队列
+    Locker m_queue_locker{};
+    Semaphore has_job{};           // 信号量——是否有任务加入工作队列
+    bool running{};   // 线程池是否正在运行
+public:
+    ThreadPool(uint32_t nb_threads = 5, uint32_t max_nb_requests = 100) 
+    :m_thread_number(nb_threads), m_max_requests(max_nb_requests), m_threads(nb_threads)
+    {
+        for (int i = 0; i < nb_threads; ++i)
+        {
+            if (pthread_create(&m_threads[i], NULL, worker, this) != 0 || pthread_detach(m_threads[i]))
+            {
+                throw std::exception();
+            }
+        }
+        running = true;
+    }
+
+    ~ThreadPool()
+    {
+        running = false;
+    }
+
+    bool append(T * job)
+    {
+
+    }
+
 };
 
 
