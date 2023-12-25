b3d3c754dc47fcd76ddc17953b13d5a49b7a5a79
rda <2055017970@qq.com>
2023-12-25 19:02:13 +0800

    add some data members

--- http/http_conn.hh
+++ http/http_conn.hh
@@ -5,7 +5,7 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
-
+#include <arpa/inet.h>
 
 /// @brief http连接类，负责处理一个http连接的请求
 class HttpConn
@@ -19,24 +19,72 @@ public:
     // 采用宏定义的格式命名，和http保持一致
     enum class Method
     {
-        GET, POST, HEAD, PUT, DELETE, TRACE,OPTIONS
+        GET,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS
     };
 
     // HTTP请求解析的状态
-    enum class CheckStatus{
-        kCheckRequestLine, kCheckHeader, kCheckContent
+    enum class CheckStatus
+    {
+        kCheckRequestLine,
+        kCheckHeader,
+        kCheckContent
     };
 
     // 返回的状态码
-    enum class HttpCode {
+    enum class HttpCode
+    {
 
     };
 
     // 解析行的状态
-    enum class LineStatus {
-        
+    enum class LineStatus
+    {
+        kLineOk,
+        kLineOpen,
+        kLineBad
     };
-    
+
+    /* 静态数据成员 */
+    // 所有socket上的事件都被注册到同一个epoll内核事件表，I/O线程（主线陈）负责监听
+    static int m_epollfd;
+
+private:
+    /* 连接相关的成员 */
+    // 当前http连接的socket描述符
+    int m_sockfd;
+    // 对端用户地址 
+    sockaddr_in m_address;
+    Method m_method;
+    char *m_url;
+    char *m_host;
+
+
+    /* 读缓冲区相关的成员 */
+    // 读缓冲区
+    char m_read_buf[kBufferSize];
+    // 下次读取数据的位置
+    int m_read_idx;
+    // 当前分析的位置
+    int m_checked_idx;
+    // 当前解析的行的起始位置
+    int m_start_line;
+
+    /* 写缓冲区相关的成员 */
+    // 写缓冲区
+    char m_write_buf[kBufferSize];
+    // 下次写入数据的位置
+    int m_write_idx;
+
+
+
+public:
+    HttpConn() = default;
 };
 
 #endif
